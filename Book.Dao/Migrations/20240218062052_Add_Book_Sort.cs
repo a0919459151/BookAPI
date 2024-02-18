39b161bc313f28a3381f863d762b5809a1433b85
Little Soft <a0919459151@gmail.com>
2024-02-18 14:21:33 +0800

    feat : db book add sort

---
+++ Book.Dao/Migrations/20240218062052_Add_Book_Sort.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Book.Dao.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Book_Sort : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Sort",
+                table: "Books",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sort",
+                table: "Books");
+        }
+    }
+}
